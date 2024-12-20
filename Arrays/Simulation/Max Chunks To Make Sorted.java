2a093ceb5ced5768ef43a7ced7463920973c3379
Abhay Sharma <shabhay007@gmail.com>
2024-12-20 15:12:57 +0530

    Removed extra approach from 'Max Chunks To Make Sorted' in arrays simulation.

--- Arrays/Simulation/Max Chunks To Make Sorted.java
+++ Arrays/Simulation/Max Chunks To Make Sorted.java
@@ -83,26 +83,3 @@ class Solution {
         return chunkCount;
     }
 }
-
-
-
-// Approach 4 - Simulation
-// T.C. - O(n)
-// S.C. - O(1)
-class Solution {
-    public int maxChunksToSorted(int[] arr) {
-        int n = arr.length;
-        int max = Integer.MIN_VALUE;
-        int chunkCount = 0;
-
-        for(int i = 0; i<n; i++){
-            max = Math.max(max, arr[i]);
-
-            if(max < i+1){
-                chunkCount++;
-            }
-        }
-
-        return chunkCount;
-    }
-}
