904e223f1a2c7b25fce98eb1f65e926114486ebf
Abhay Sharma <shabhay007@gmail.com>
2025-03-14 16:30:01 +0530

    Added 2 more methods (Java Stream API and optimised Binary Search) for the problem 'Maximum Count of Positive Integer and Negative Integer' in Binary Search Easy.

--- Arrays/Binary Search/Easy/Maximum Count of Positive Integer and Negative Integer.java
+++ Arrays/Binary Search/Easy/Maximum Count of Positive Integer and Negative Integer.java
@@ -89,3 +89,62 @@ class Solution {
         return Math.max(totalNegNumbers, totalPosNumbers);
     }
 }
+
+
+
+// Approach 3 - Java Stream API
+// T.C. - O(2n)
+// S.C. - O(1)
+class Solution {
+    public int maximumCount(int[] nums) {
+        IntPredicate lambdaP = num -> num > 0;
+        IntPredicate lambdaN = num -> num < 0;
+
+        // by default count(), here returns in long
+        int positiveCount = (int) Arrays.stream(nums).filter(lambdaP).count();
+        int negativeCount = (int) Arrays.stream(nums).filter(lambdaN).count();
+
+        return Math.max(positiveCount, negativeCount);
+    }
+}
+
+
+
+
+// Approach 4 - Binary Search
+// T.C. - O(2log(n))
+// S.C. - O(1)
+class Solution {
+    public int lowerBound(int[] nums, int n, int target){
+        int idx = n; // to avoid case when all 0's
+
+        int l = 0;
+        int r = n-1;
+
+        while(l <= r){
+            int mid = l + (r - l)/2;
+
+            if(nums[mid] >= target){
+                idx = mid;
+                r = mid - 1;
+            }
+            else{
+                l = mid + 1;
+            }
+        }
+
+        return idx;
+    }
+
+    public int maximumCount(int[] nums) {
+        int n = nums.length;
+
+        // 1st positive number idx i.e. num >= 1
+        int positiveCount = lowerBound(nums, n, 1);
+
+        // 1st 0 or positive number idx i.e. num >= 0
+        int negativeCount = lowerBound(nums, n, 0);
+
+        return Math.max(n - positiveCount, negativeCount); // Max(n - n, 0)
+    }
+}
