1e8737c9b7bcd12adedea9c420330dcd7c96ce7b
Abhay Sharma <shabhay007@gmail.com>
2025-03-18 00:10:46 +0530

    Added 2 more approaches for the problem 'Alternating Groups II' in Sliding Window.

--- Arrays/Sliding Window/Alternating Groups II.java
+++ Arrays/Sliding Window/Alternating Groups II.java
@@ -28,3 +28,111 @@ class Solution {
         return result;
     }
 }
+
+
+
+
+// Approach 2 -> 2 pass
+// T.C. - O(n + k)
+// S.C. - O(1)
+class Solution {
+    public int numberOfAlternatingGroups(int[] colors, int k) {
+        int n = colors.length;
+
+        int length = 1; // element at index 0.
+        int lastColor = colors[0];
+        int groups = 0;
+
+        // 1st pass
+        for(int i = 1; i<n; i++){
+            if(colors[i] == lastColor){
+                length = 1;
+                lastColor = colors[i];
+                continue;
+            }
+
+            length++;
+
+            if(length >= k){
+                groups++;
+            }
+
+            lastColor = colors[i];
+        }
+
+        // 2nd pass
+        for(int i = 0; i < k-1; i++){ // for k-1 starting elements
+            if(colors[i] == lastColor){
+                length = 1;
+                lastColor = colors[i];
+                continue;
+            }
+
+            length++;
+
+            if(length >= k){
+                groups++;
+            }
+
+            lastColor = colors[i];
+        }
+
+        return groups;
+    }
+}
+
+
+
+
+// Approach 3 -> 2 pass with slight optimization in Approach 2
+// T.C. - O(n + k)
+// S.C. - O(1)
+class Solution {
+    public int numberOfAlternatingGroups(int[] colors, int k) {
+        int n = colors.length;
+
+        int length = 1; // element at index 0.
+        int lastColor = colors[0];
+        int groups = 0;
+
+        // 1st pass
+        for(int i = 1; i<n; i++){
+            if(colors[i] == lastColor){
+                length = 1;
+                lastColor = colors[i];
+                continue;
+            }
+
+            length++;
+
+            if(length >= k){
+                groups++;
+            }
+
+            lastColor = colors[i];
+        }
+
+        // 2nd pass
+        for(int i = 0; i < k-1; i++){ // for k-1 starting elements
+            if(colors[i] == lastColor){
+                length = 1;
+                lastColor = colors[i];
+
+                // since we are checking here only k elements and 1 elements
+                // is equal, that means we'll never get k sized window
+                // so we can use break instead of continue;
+                break; // only change
+            }
+
+            length++;
+
+            if(length >= k){
+                groups++;
+            }
+
+            lastColor = colors[i];
+        }
+
+        return groups;
+    }
+}
