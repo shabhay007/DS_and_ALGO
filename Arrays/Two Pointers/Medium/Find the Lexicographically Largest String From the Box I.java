8cb0f70b73ce7a059a519321bc45800ca5b6eec9
Abhay Sharma <shabhay007@gmail.com>
2025-06-05 00:27:10 +0530

    Added 2nd optimised approach for the problem 'Find the Lexicographically Largest String From the Box I' in Arrays Two Pointer.

--- Arrays/Two Pointers/Medium/Find the Lexicographically Largest String From the Box I.java
+++ Arrays/Two Pointers/Medium/Find the Lexicographically Largest String From the Box I.java
@@ -4,7 +4,7 @@
 
 // Approach 1
 // T.C. - O(n^2)
-// S.C. - O(n); max length of result string can be n
+// S.C. - O(1); no extra space; only O(n) which is req. to return as result
 class Solution {
     public String answerString(String word, int numFriends) {
         int n = word.length();
@@ -23,6 +23,55 @@ class Solution {
                 result = current;
             }
         }
+
+        return result;
+    }
+}
+
+
+
+
+
+// Approach 2
+// T.C. - O(n)
+// S.C. - O(1); no extra space; only O(n) which is req. to return as result
+class Solution {
+    public int bestStartingPoint(String word, int n){
+        int i = 0; // best starting point
+        int j = 1; // it will move to find the best starting point
+
+        while(j < n){
+            int k = 0;
+
+            // skipping equal characters
+            // no need to check i+k < n as we are checking j+k < n, and j > i
+            while(j + k < n && word.charAt(i + k) == word.charAt(j + k)){
+                k++;
+            }
+
+            if(j + k < n && word.charAt(j + k) > word.charAt(i + k)){
+                i = j; // updating best starting point
+                j = j+1;
+            }
+            else{
+                j = j + k + 1; // skipping already checked characters
+            }
+        }
+
+        return i;
+    }
+
+    public String answerString(String word, int numFriends) {
+        int n = word.length();
+
+        if(numFriends == 1){
+            return word;
+        }
+
+        int position = bestStartingPoint(word, n);
+        int maxPossibleLen = n - (numFriends - 1);
+
+        String result = word.substring(position, Math.min(n, position + maxPossibleLen));
 
         return result;
     }
