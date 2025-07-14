3bd1c66243104710e15d88f3d2b0fab99525794b
Abhay Sharma <shabhay007@gmail.com>
2025-07-14 23:38:17 +0530

    Added problem 'Convert Binary Number in a Linked List to Integer' in Linked List Easy

---
+++ Linked List/Easy/Convert Binary Number in a Linked List to Integer.java
@@ -0,0 +1,26 @@
+// LeetCode - 1290
+
+
+
+// Approach 1 - By parsing binary String to decimal integer
+ // T.C. - O(n); n = total no of nodes
+ // S.C. - O(n)
+ class Solution {
+    public int getDecimalValue(ListNode head) {
+        if(head == null){
+            return 0;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        ListNode temp = head;
+
+        while(temp != null){ // O(n)
+            sb.append(temp.val);
+            temp = temp.next;
+        }
+
+        int decimalNumber = Integer.parseInt(sb.toString(), 2); // O(n)
+
+        return decimalNumber;
+    }
+}
