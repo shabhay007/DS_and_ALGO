c2216b0f6814b6b643bdde3e6ae90c07f67c3dcb
Abhay Sharma <shabhay007@gmail.com>
2025-07-15 23:27:36 +0530

    Added problem 'Valid Word' in String Easy

---
+++ Strings/String Easy/Valid Word.java
@@ -0,0 +1,33 @@
+// LeetCode - 3136
+
+
+
+
+// Approach (simple traverse and check)
+// T.C : O(n)
+// S.C : O(1)
+class Solution {
+    public boolean isValid(String word) {
+        if (word.length() < 3) {
+            return false;
+        }
+
+        boolean hasVowel = false;
+        boolean hasConsonant = false;
+
+        for (char ch : word.toCharArray()) {
+            if (Character.isLetter(ch)) {
+                ch = Character.toLowerCase(ch);
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    hasVowel = true;
+                } else {
+                    hasConsonant = true;
+                }
+            } else if (!Character.isDigit(ch)) {
+                return false;
+            }
+        }
+
+        return hasVowel && hasConsonant;
+    }
+}
