b1ebfa5589c110f3bcba4f2dd7db7b8ecf04cff7
Abhay Sharma <shabhay007@gmail.com>
2025-07-22 13:56:07 +0530

    Added another approach to the problem 'Maximum Erasure Value' in Sliding Window Medium

--- Arrays/Sliding Window/Medium/Maximum Erasure Value.java
+++ Arrays/Sliding Window/Medium/Maximum Erasure Value.java
@@ -33,3 +33,44 @@ class Solution {
         return maxSum;
     }
 }
+
+
+
+
+
+// Approach 2 - Sliding Window + Cummulative Sum + Hash Array/Map
+// T.C. - O(3n)
+// S.C. - O(2n)
+class Solution {
+    public int maximumUniqueSubarray(int[] nums) {
+        int n = nums.length;
+
+        int[] cummSum = new int[n];
+        cummSum[0] = nums[0];
+
+        for(int i = 1; i<n; i++){
+            cummSum[i] = nums[i] + cummSum[i-1];
+        }
+
+        int[] map = new int[10001];
+        Arrays.fill(map, -1);
+
+        int i = 0;
+        int j = 0;
+        int maxSum = 0;
+
+        while(j < n){
+            // jumping i so that i - j is valid subarray
+            i = Math.max(i, map[nums[j]] + 1);
+            int jthSum = cummSum[j];
+            int ithSum = (i - 1) < 0 ? 0 : cummSum[i-1];
+            int subarraySum = jthSum - ithSum;
+            maxSum = Math.max(maxSum, subarraySum);
+            map[nums[j]] = j;
+
+            j++;
+        }
+
+        return maxSum;
+    }
+}
