c7c04875e9b5c44087329ea84b48313e39855133
Abhay Sharma <shabhay007@gmail.com>
2025-07-30 16:58:42 +0530

    Added other approaches to the problem 'Longest Subarray With Maximum Bitwise AND'

--- Bit Manipulation/Medium/Longest Subarray With Maximum Bitwise AND.java
+++ Bit Manipulation/Medium/Longest Subarray With Maximum Bitwise AND.java
@@ -2,7 +2,10 @@
 
 
 
-// Approach 1 - Two Pointer/Sliding Window (It is solved because of Bit Manipulation concept)
+
+// It is solved because of Bit Manipulation concept
+
+// Approach 1 - Two Pointer/Sliding Window
 // T.C. - O(2n)
 // S.C. - O(1)
 class Solution {
@@ -27,6 +30,75 @@ class Solution {
 
             j++;
             i = j;
+        }
+
+        return maxLen;
+    }
+}
+
+
+
+
+
+// Approach 2
+// T.C. - O(2n)
+// S.C. - O(1)
+class Solution {
+    public int longestSubarray(int[] nums) {
+        int n = nums.length;
+        int max = 0;
+
+        for(int i = 0; i<n; i++){
+            max = Math.max(max, nums[i]);
+        }
+
+        int count = 0;
+        int length = 0;
+
+        for(int i = 0; i<n; i++){
+            if(nums[i] == max){
+                count++;
+                length = Math.max(length, count);
+            }
+            else{
+                count = 0;
+            }
+        }
+
+        return length;
+    }
+}
+
+
+
+
+
+
+// Approach 3
+// T.C. - O(n)
+// S.C. - O(1)
+class Solution {
+    public int longestSubarray(int[] nums) {
+        int n = nums.length;
+        int currLen = 0;
+        int maxLen = 0;
+        int maxVal = nums[0];
+
+        for(int i = 0; i<n; i++){
+            if(nums[i] > maxVal){
+                maxVal = nums[i];
+                currLen = 1;
+                maxLen = currLen;
+
+            }
+            else if(nums[i] == maxVal){
+                currLen++;
+            }
+            else{
+                currLen = 0;
+            }
+
+            maxLen = Math.max(maxLen, currLen);
         }
 
         return maxLen;
