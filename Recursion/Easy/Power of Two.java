0675c24cf2cf8bfe0777f5f30f03d34748acf0f9
Abhay Sharma <shabhay007@gmail.com>
2025-08-10 00:47:03 +0530

    Added problem 'Power of Two' in Recursion Easy

--- Recursion/Easy/Power of Two.java
+++ Recursion/Easy/Power of Two.java
@@ -41,3 +41,63 @@ class Solution {
         return isPowerOfTwo(n/2);
     }
 }
+
+
+
+
+
+// Approach 3 - Bit Manipulation
+// T.C. - O(1)
+// S.C. - O(1)
+
+/*
+    If n is power of 2, then there will only be one set bit
+    eg. 2 - 10,  16, - 10000, ....
+
+    So, when we perform & opration between n and (n-1) (not power of two), 
+    then because of n-1, all the bits will be 0
+
+    And we can say that n is power of 2.
+*/
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n <= 0){
+            return false;
+        }
+
+        return (n & (n-1)) == 0;
+    }
+}
+
+
+
+
+
+
+// Approach 4 - Bit Manipulation
+// T.C. - O(log(n))
+// S.C. - O(1)
+
+/*
+    If n is power of 2, then there will only be one set bit
+    eg. 2 - 10,  16, - 10000, ....
+
+    So, when we perform & opration between n and (n-1) (not power of two), 
+    then because of n-1, all the bits will be 0
+
+    And we can say that n is power of 2.
+
+    Because of this same concept, we can also say that, if a number has only
+    1 set bit then it will be power of 2, else not
+*/
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n <= 0){
+            return false;
+        }
+
+        return Integer.bitCount(n) == 1; // internally it will take O(log(n))
+    }
+}
