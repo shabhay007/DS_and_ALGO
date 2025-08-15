484bdc2d2f246c5dffb352b42b2b07b8292fab3e
Abhay Sharma <shabhay007@gmail.com>
2025-08-15 14:24:08 +0530

    Added other approach to the problem 'Power of Four'

--- Recursion/Easy/Power of Four.java
+++ Recursion/Easy/Power of Four.java
@@ -61,3 +61,19 @@ class Solution {
         return x == (int) x;
     }
 }
+
+
+
+
+// Approach 4 - Bit Manipulation + Maths
+// T.C. - O(1)
+// S.C. - O(1)
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n <= 0){
+            return false;
+        }
+        
+        return ((n & (n-1)) == 0) && (n-1) % 3 == 0;
+    }
+}
