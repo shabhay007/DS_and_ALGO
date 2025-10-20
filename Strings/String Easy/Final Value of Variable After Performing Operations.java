e1f40e69a97e56884455ac039cce895176f2b32d
Abhay Kumar Sharma <abhay.sharma@corp.csmpl.com>
2025-10-20 15:28:37 +0530

    Added other approach in 'Final Value of Variable After Performing Operations' in String Easy

--- Strings/String Easy/Final Value of Variable After Performing Operations.java
+++ Strings/String Easy/Final Value of Variable After Performing Operations.java
@@ -21,3 +21,27 @@ class Solution {
         return result;
     }
 }
+
+
+
+
+
+// Approach 2 - Simulation
+// T.C. - O(n)
+// S.C. - O(1)
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int result = 0;
+
+        for(String str : operations){
+            if(str.charAt(1) == '+'){ // constant time check
+                result++;
+            }
+            else{
+                result--;
+            }
+        }
+
+        return result;
+    }
+}
