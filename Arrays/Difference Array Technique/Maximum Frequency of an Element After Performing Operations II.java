a834bd163da8dd7a80e1bebb44feab5453debbc7
Abhay Kumar Sharma <abhay.sharma@corp.csmpl.com>
2025-10-22 20:02:31 +0530

    Added problem 'Maximum Frequency of an Element After Performing Operations II' in Difference Array in Arrays

--- Arrays/Difference Array Technique/Maximum Frequency of an Element After Performing Operations II.java
+++ Arrays/Difference Array Technique/Maximum Frequency of an Element After Performing Operations II.java
@@ -1,8 +1,8 @@
-// LeetCode - 3346
+// LeetCode - 3347
 
 
 
-// Approach 1 - Prefix Sum
+// Approach 1 - Prefix Sum (Gives MLE)
 // T.C. - O(n)
 // S.C. - O(n)
 class Solution {
@@ -60,7 +60,7 @@ class Solution {
 
 
 
-// Approach 2 - Difference Array Technique
+// Approach 2 - Difference Array Technique (Gives MLE)
 // T.C. - O(n); n = max + k
 // S.C. - O(n)
 class Solution {
@@ -94,6 +94,53 @@ class Solution {
             int maxPossibleConversion = Math.min(needConversion, numOperations);
 
             result = Math.max(result, maxPossibleConversion + targetFreq);
+        }
+
+        return result;
+    }
+}
+
+
+
+
+
+// Approach 3 - Using Difference Array Technique
+// T.C : O(nlogn)
+// S.C : O(n)
+class Solution {
+    public int maxFrequency(int[] nums, int k, int numOperations) {
+        int maxVal = Arrays.stream(nums).max().getAsInt() + k;
+        TreeMap<Integer, Integer> diff = new TreeMap<>();
+        HashMap<Integer, Integer> freq = new HashMap<>();
+
+        // O(n log n)
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+
+            int l = Math.max(num - k, 0);
+            int r = Math.min(num + k, maxVal);
+
+            diff.put(l, diff.getOrDefault(l, 0) + 1);
+            diff.put(r + 1, diff.getOrDefault(r + 1, 0) - 1);
+
+            // Ensure key exists for later iteration
+            diff.putIfAbsent(num, diff.getOrDefault(num, 0));
+        }
+
+        int result = 1;
+        int cumSum = 0;
+
+        // O(n)
+        for (Map.Entry<Integer, Integer> entry : diff.entrySet()) {
+            int target = entry.getKey();
+            int val = entry.getValue();
+
+            cumSum += val;
+            int targetFreq = freq.getOrDefault(target, 0);
+            int needConversion = cumSum - targetFreq;
+
+            int maxPossibleFreq = Math.min(needConversion, numOperations);
+            result = Math.max(result, targetFreq + maxPossibleFreq);
         }
 
         return result;
