67eae902a8275b607820a6f991d1c746c7962889
Abhay Kumar Sharma <abhay.sharma@corp.csmpl.com>
2025-10-29 10:16:35 +0530

    Added other approaches to 'Smallest Number With All Set Bits'

--- Bit Manipulation/Easy/Smallest Number With All Set Bits.java
+++ Bit Manipulation/Easy/Smallest Number With All Set Bits.java
@@ -29,3 +29,52 @@ class Solution {
         return Integer.parseInt(newStr, 2);
     }
 }
+
+
+
+
+
+// Approach 2 - Bit Manipulation
+// T.C. - O(log(n))
+// S.C. - O(log(n))
+class Solution {
+    public int smallestNumber(int n) {
+        String binaryStr = Integer.toBinaryString(n); // O(log(n))
+        int len = binaryStr.length();
+
+        return (int) (Math.pow(2, len)) - 1;
+    }
+}
+
+
+
+
+
+
+// Approach 3 - Bit Manipulation (Minor change in Approach 2)
+// T.C. - O(log(n))
+// S.C. - O(log(n))
+class Solution {
+    public int smallestNumber(int n) {
+        String binaryStr = Integer.toBinaryString(n); // O(log(n))
+        int len = binaryStr.length();
+
+        return (1 << len) - 1;
+    }
+}
+
+
+
+
+
+
+// Approach 4 - Optimised Bit Manipulation
+// T.C. - O(1)
+// S.C. - O(1)
+class Solution {
+    public int smallestNumber(int n) {
+        int len = (int) (Math.log(n) / Math.log(2)) + 1; // binary length
+
+        return (1 << len) - 1;
+    }
+}
