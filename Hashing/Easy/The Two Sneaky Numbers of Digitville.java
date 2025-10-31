3ea7f0ab355829caed5d05ec15b2b48a1b70e2e5
Abhay Sharma <shabhay007@gmail.com>
2025-10-31 18:08:43 +0530

    Added other approach to the problem 'The Two Sneaky Numbers of Digitville'

--- Hashing/Easy/The Two Sneaky Numbers of Digitville.java
+++ Hashing/Easy/The Two Sneaky Numbers of Digitville.java
@@ -55,3 +55,59 @@ class Solution {
         return result;
     }
 }
+
+
+
+
+
+// Approach 3 - Bit Manipulation
+// T.C. - O(n)
+// S.C. - O(1)
+class Solution {
+    public int[] getSneakyNumbers(int[] nums) {
+        int n = nums.length - 2;
+
+        int xor = 0;
+
+        // xor of nums
+        for(int num : nums){
+            xor ^= num;
+        }
+
+        // org list, xor from 0 to n-1 as length is n
+        for(int num = 0; num < n; num++){
+            xor ^= num;
+        }
+
+        // finding trailing zero in xor to find separator
+        int trailZeroCount = Integer.numberOfTrailingZeros(xor);
+        
+        // filter/mask
+        int mask = 1 << trailZeroCount;
+
+        // separating into two groups based on set bit
+
+        int group1 = 0;
+        int group2 = 0;
+
+        for(int num : nums){
+            if((num & mask) != 0){
+                group1 ^= num;
+            }
+            else{
+                group2 ^= num;
+            }
+        }
+
+        for(int num = 0; num < n; num++){
+            if((num & mask) != 0){
+                group1 ^= num;
+            }
+            else{
+                group2 ^= num;
+            }
+        }
+
+        return new int[]{group1, group2};
+    }
+}
