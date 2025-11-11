dffc1333cd1d274d6874e9d06bc56d9f57185663
Abhay Sharma <shabhay007@gmail.com>
2025-11-11 21:30:50 +0530

    Added other approach to the problem 'Ones and Zeroes' in DP on Strings

--- DP/DP on Strings/Ones and Zeroes.java
+++ DP/DP on Strings/Ones and Zeroes.java
@@ -2,7 +2,7 @@
 
 
 
-// Approach 1 - Recursion (Brute Force)
+// Approach 1 - Recursion (Brute Force - Gives TLE)
 // T.C. - O(2^n * len); len = avg length of strings
 // S.C. - O(strs.length)
 class Solution {
@@ -106,5 +106,66 @@ class Solution {
         int oneCount = 0;
 
         return getMaxLength(0, strs, m, n, zeroCount, oneCount, dp);
+    }
+}
+
+
+
+
+
+
+// Approach 3 - Recursion + Memoization + Precomputation of strings
+// T.C. - O(L * m * n * len); L = length(strs), len = avg length of strings
+// S.C. - O(L * m * n)
+class Solution {
+    public int getMaxLength(int i, List<int[]> list, int m, int n, int[][][] dp){
+        if(i >= list.size() || (m == 0 && n == 0)){
+            return 0;
+        }
+
+        if(dp[i][m][n] != -1){
+            return dp[i][m][n];
+        }
+
+        int take = 0;
+        
+        if((list.get(i)[0] <= m) && (list.get(i)[1] <= n)){
+            take = 1 + getMaxLength(i+1, list, m - list.get(i)[0], n - list.get(i)[1], dp);
+        }
+
+        int skip = getMaxLength(i+1, list, m, n, dp);
+
+        return dp[i][m][n] = Math.max(take, skip);
+    }
+
+    public int findMaxForm(String[] strs, int m, int n) {
+        int L = strs.length;
+        List<int[]> list = new ArrayList<>();
+
+        for(int i = 0; i<L; i++){
+            int zero = 0;
+            int one = 0;
+
+            for(char ch : strs[i].toCharArray()){
+                if(ch == '0'){
+                    zero++;
+                }
+                else{
+                    one++;
+                }
+            }
+
+            list.add(new int[]{zero, one});
+        }
+
+        int[][][] dp = new int[strs.length + 1][m + 1][n + 1];
+        
+        for(int[][] twoD : dp){
+            for(int[] oneD : twoD){
+                Arrays.fill(oneD, -1);
+            }
+        }
+
+        return getMaxLength(0, list, m, n, dp);
     }
 }
