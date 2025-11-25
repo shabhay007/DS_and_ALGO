ab67ebf562cb647efc9066f9c4703b7649895c93
Abhay Sharma <shabhay007@gmail.com>
2025-11-25 21:11:59 +0530

    Added other approach to the problem 'Smallest Integer Divisible by K'

--- Maths/Medium/Smallest Integer Divisible by K.java
+++ Maths/Medium/Smallest Integer Divisible by K.java
@@ -27,3 +27,34 @@ class Solution {
         return length;
     }
 }
+
+
+
+
+
+
+// Approach 2 - Maths
+// T.C. - O(k)
+// S.C. - O(1)
+class Solution {
+    public int smallestRepunitDivByK(int k) {
+        if(k == 1){
+            return 1;
+        }
+
+        int num = 0;
+
+        // length will be from 1 to k-1
+        for(int length = 1; length <= k; length++){
+            // remainder will never exceed k-1
+            // and to avoid overflow, using modulo
+            num = (num * 10 + 1) % k;
+
+            if(num == 0){
+                return length;
+            }
+        }
+
+        return -1;
+    }
+}
