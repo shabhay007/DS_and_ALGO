017ece16478ac9bf3f60d8c7c6383205c6359cac
Abhay Sharma <shabhay007@gmail.com>
2025-11-26 20:33:38 +0530

    Added problem 'Paths in Matrix Whose Sum Is Divisible by K' in Dp on grids

---
+++ DP/DP on Grids/Paths in Matrix Whose Sum Is Divisible by K.java
@@ -0,0 +1,84 @@
+// LeetCode Hard - 2435
+
+
+
+// Approach 1 - DFS
+// T.C. - O(2^(m+n))
+// S.C. - O(m+n) // recursion depth
+class Solution {
+    int mod = (int) 1e9 + 7;
+
+    public int getPaths(int i, int j, int currRem, int[][] grid, int k, int m, int n){
+        if(i == m || j == n){
+            return 0;
+        }
+
+        if(i == m-1 && j == n-1){
+            int rem = (currRem + grid[i][j]) % k;
+
+            return rem == 0 ? 1 : 0;
+        }
+
+        int newRem = (currRem + grid[i][j]) % k;
+
+        int right = getPaths(i+1, j, newRem, grid, k, m, n);
+        int down =  getPaths(i, j+1, newRem, grid, k, m, n);
+
+        return (right + down) % mod;
+    }
+
+    public int numberOfPaths(int[][] grid, int k) {
+        int m = grid.length;
+        int n = grid[0].length;
+        return getPaths(0, 0, 0, grid, k, m, n);
+    }
+}
+
+
+
+
+
+
+// Approach 2 - DP (Memoization)
+// T.C. - O(m*n*k)
+// S.C. - O(m*n*k)
+class Solution {
+    int mod = (int) 1e9 + 7;
+
+    public int getPaths(int i, int j, int currRem, int[][] grid, int k, int m, int n, int[][][] dp){
+        if(i == m || j == n){
+            return 0;
+        }
+
+        if(dp[i][j][currRem] != -1){
+            return dp[i][j][currRem];
+        }
+
+        if(i == m-1 && j == n-1){
+            int rem = (currRem + grid[i][j]) % k;
+
+            return rem == 0 ? 1 : 0;
+        }
+
+        int newRem = (currRem + grid[i][j]) % k;
+
+        int right = getPaths(i+1, j, newRem, grid, k, m, n, dp);
+        int down =  getPaths(i, j+1, newRem, grid, k, m, n, dp);
+
+        return dp[i][j][currRem] = (right + down) % mod;
+    }
+
+    public int numberOfPaths(int[][] grid, int k) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][][] dp = new int[m+1][n+1][k];
+
+        for(int[][] mat : dp){
+            for(int[] row : mat){
+                Arrays.fill(row, -1);
+            }
+        }
+
+        return getPaths(0, 0, 0, grid, k, m, n, dp);
+    }
+}
