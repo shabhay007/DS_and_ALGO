57832e99b394323d75a96748767cd8028e9cb9fb
Abhay Sharma <shabhay007@gmail.com>
2026-01-20 15:22:04 +0530

    Added optimal approach to the problem 'Construct the Minimum Bitwise Array I'

--- Bit Manipulation/Easy/Construct the Minimum Bitwise Array I.java
+++ Bit Manipulation/Easy/Construct the Minimum Bitwise Array I.java
@@ -27,3 +27,45 @@ class Solution {
         return result;
     }
 }
+
+
+
+
+
+// Approach 2 - Bit Manipulation
+// T.C. - O(n * 32)
+// S.C. - O(1)
+class Solution {
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int n = nums.size();
+        int[] result = new int[n];
+
+        for(int i = 0; i<n; i++){
+            int num = nums.get(i);
+
+            if(num == 2){
+                result[i] = -1;
+                continue;
+            }
+
+            int ans = -1;
+
+            for(int j = 1; j<32; j++){
+                // if jth bit is set, skip
+                if((num & (1 << j)) != 0){
+                    continue;
+                }
+
+                // found an unset bit at position j
+                // flipping (j-1)th bit
+                // making previous bit (most significant) 0 in order to get the min x
+                int prev = j-1;
+                ans = num ^ (1 << (prev));
+            }
+
+            result[i] = ans;
+        }
+        
+        return result;
+    }
+}
