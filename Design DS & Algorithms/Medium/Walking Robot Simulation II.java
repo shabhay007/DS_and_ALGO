541ad18bc4dad6eef67eb6c2650d726016b4a20b
Abhay Sharma <shabhay007@gmail.com>
2026-04-07 19:43:48 +0530

    Added other approach to the problem 'Walking Robot Simulation II'

--- Design DS & Algorithms/Medium/Walking Robot Simulation II.java
+++ Design DS & Algorithms/Medium/Walking Robot Simulation II.java
@@ -124,3 +124,67 @@ class Robot {
         }
     }
 }
+
+
+
+
+
+
+// Approach 3 - Pre computing all position with directions robot can move + Simulation
+// T.C. - O(height *+ width)
+// S.C. - O(2 * height * width - 4)
+class Robot {
+    int idx = 0;
+    boolean moved = false; // to handle 1 corner case
+    List<int[]> list; // to store all boundary cells with directions
+
+    public Robot(int width, int height) {
+        this.list = new ArrayList<>();
+
+        // Bottom row (left to right) -> East(0)
+        for(int x = 0; x<width; x++){
+            list.add(new int[]{x, 0, 0});
+        }
+
+        // Right col (bottom to top) -> North(1)
+        for(int y = 1; y<height; y++){
+            list.add(new int[]{width-1, y, 1});
+        }
+
+        // Top row (right to left) -> West(2)
+        for(int x = width - 2; x >= 0; x--){
+            list.add(new int[]{x, height - 1, 2});
+        }
+
+        // Left col (top to bottom) -> South(3)
+        for(int y = height - 2; y > 0; y--){ // we'll handle (0) case separately
+            list.add(new int[]{0, y, 3});
+        }
+        
+        // list = {{0, 0, 0}, ....}
+        // Making it south for handling corner case when it comes to (0, 0) after moving
+        list.get(0)[2] = 3;
+    }
+    
+    public void step(int num) {
+        moved = true;
+        idx = (idx + num) % list.size();
+    }
+    
+    public int[] getPos() {
+        return new int[]{list.get(idx)[0], list.get(idx)[1]};
+    }
+    
+    public String getDir() {
+        if(!moved){
+            return "East";
+        }
+
+        int dir = list.get(idx)[2];
+
+        if(dir == 0) return "East";
+        else if(dir == 1) return "North";
+        else if(dir == 2) return "West";
+        else return "South";
+    }
+}
