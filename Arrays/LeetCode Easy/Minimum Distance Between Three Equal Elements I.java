7da506df58e9728a73436b54eab1b56e7ffba9c8
Abhay Sharma <shabhay007@gmail.com>
2026-04-10 10:32:43 +0530

    Added other optimized approach to the problem 'Minimum Distance Between Three Equal Elements I'

--- Arrays/LeetCode Easy/Minimum Distance Between Three Equal Elements I.java
+++ Arrays/LeetCode Easy/Minimum Distance Between Three Equal Elements I.java
@@ -24,3 +24,41 @@ class Solution {
         return minDistance == Integer.MAX_VALUE ? -1 : minDistance;
     }
 }
+
+
+
+
+
+
+// Approach 2 - Map
+// T.C. - O(n)
+// S.C. - O(n)
+class Solution {
+    public int minimumDistance(int[] nums) {
+        int n = nums.length;
+        Map<Integer, List<Integer>> map = new HashMap<>();
+
+        for(int i = 0; i<n; i++){
+            map.putIfAbsent(nums[i], new ArrayList<>());
+            map.get(nums[i]).add(i);
+        }
+
+        int minDistance = Integer.MAX_VALUE;
+        for(List<Integer> list : map.values()){
+            if(list.size() < 3){
+                continue;
+            }
+
+            for(int l = 0; l<list.size() - 2; l++){
+                int i = list.get(l);
+                int j = list.get(l+1);
+                int k = list.get(l+2);
+                int curr = k - i + k - j + j - i;
+                minDistance = Math.min(minDistance, curr);
+            }
+
+        }
+
+        return minDistance == Integer.MAX_VALUE ? -1 : minDistance;
+    }
+}
