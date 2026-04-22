abcc5fcbd249d4e9458071f2f1c98b8949da56ec
Abhay Sharma <shabhay007@gmail.com>
2026-04-22 14:59:37 +0530

    Added other approach to the problem 'Words Within Two Edits of Dictionary'

--- Strings/Medium/Words Within Two Edits of Dictionary.java
+++ Strings/Medium/Words Within Two Edits of Dictionary.java
@@ -33,3 +33,44 @@ class Solution {
         return result;
     }
 }
+
+
+
+
+
+
+// Approach 2 - Brute Force (Slight Optimization using Early break)
+// T.C. - O(m * n * L); m = queries.length, n = dict.length, L = avg. length or words
+// S.C. - O(1)
+class Solution {
+    public List<String> twoEditWords(String[] queries, String[] dictionary) {
+        List<String> result = new ArrayList<>();
+
+        for(String str : queries){
+
+            // checking in dictionary
+            for(String dictStr : dictionary){
+                int diff = 0;
+
+                // matching characters in words
+                for(int i = 0; i < str.length(); i++){
+                    if(str.charAt(i) != dictStr.charAt(i)){
+                        diff++;
+                    }
+
+                    if(diff > 2){ // early break
+                        break;
+                    }
+                }
+
+                // checking if max edits <= 2
+                if(diff <= 2){
+                    result.add(str);
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
