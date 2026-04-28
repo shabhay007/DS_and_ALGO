79b0fcf7be1af6d08fd0ce4fd85a1b81c41ecf02
Abhay Sharma <shabhay007@gmail.com>
2026-04-28 18:18:39 +0530

    Added other approach to the problem 'Detect Cycles in 2D Grid'

--- Graph/BFS_DFS/Detect Cycles in 2D Grid.java
+++ Graph/BFS_DFS/Detect Cycles in 2D Grid.java
@@ -51,3 +51,70 @@ class Solution {
         return false;
     }
 }
+
+
+
+
+
+
+
+// Approach 2 - BFS
+// T.C. - O(m * n)
+// S.C. - O(m * n)
+class Solution {
+    int m;
+    int n;
+    int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    public boolean bfs(int i, int j, char[][] grid, boolean[][] visited){
+        Queue<int[]> q = new LinkedList<>();
+
+        // {curr_i, curr_j, prev_i, prev_j}
+        q.add(new int[]{i, j, -1, -1});
+        visited[i][j] = true;
+
+        while(!q.isEmpty()){
+            int[] curr = q.poll();
+            int x = curr[0];
+            int y = curr[1];
+            int prevX = curr[2];
+            int prevY = curr[3];
+
+            for(int[] dir : directions){
+                int ni = x + dir[0];
+                int nj = y + dir[1];
+                
+                if(ni >= 0 && ni < m && nj >= 0 && nj < n && grid[ni][nj] == grid[x][y]){
+                    if(ni == prevX && nj == prevY){
+                        continue;
+                    }
+
+                    if(visited[ni][nj]){
+                        return true;
+                    }
+
+                    q.offer(new int[]{ni, nj, x, y});
+                    visited[ni][nj] = true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public boolean containsCycle(char[][] grid) {
+        m = grid.length;
+        n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
+
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                if(!visited[i][j] && bfs(i, j, grid, visited)){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
