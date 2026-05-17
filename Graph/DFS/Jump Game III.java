9b5b6f37f4a9a6fb217bb5d2561343aa1b75569c
Abhay Sharma <shabhay007@gmail.com>
2026-05-17 16:20:02 +0530

    Added other approach to the problem 'Jump Game III'

--- Graph/DFS/Jump Game III.java
+++ Graph/DFS/Jump Game III.java
@@ -27,3 +27,48 @@ class Solution {
         return solve(start, arr, n, visited);
     }
 }
+
+
+
+
+
+// Approach 2 - BFS
+// T.C. - O(n)
+// S.C. - O(n)
+// Approach 2 - BFS
+// T.C. - O(n)
+// S.C. - O(n)
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        int m = arr.length;
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] visited = new boolean[m];
+
+        q.add(start);
+        visited[start] = true;
+
+        while(!q.isEmpty()){
+            int i = q.poll();
+            int num = arr[i];
+
+            if(num == 0){
+                return true;
+            }
+
+            int n1 = i + num;
+            int n2 = i - num;
+
+            if(n1 >= 0 && n1 < m && !visited[n1]){
+                q.offer(n1);
+                visited[n1] = true;
+            }
+
+            if(n2 >= 0 && n2 < m && !visited[n2]){
+                q.offer(n2);
+                visited[n2] = true;
+            }
+        }
+
+        return false;
+    }
+}
