8f1a323596194544445263e0d35048ed6f3f6f2d
Abhay Sharma <shabhay007@gmail.com>
2026-05-19 11:23:15 +0530

    Added other approach to the problem 'Minimum Common Value'

--- Arrays/Two Pointers/Easy/Minimum Common Value.java
+++ Arrays/Two Pointers/Easy/Minimum Common Value.java
@@ -3,7 +3,7 @@
 
 
 // Approach 1 - Two Pointers
-// T.C. - O(n)
+// T.C. - O(m + n)
 // S.C. - O(1)
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
@@ -41,7 +41,7 @@ class Solution {
 
 
 // Approach 2 - Two Pointers
-// T.C. - O(n)
+// T.C. - O(m + n)
 // S.C. - O(1)
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
@@ -59,6 +59,57 @@ class Solution {
             }
             else{
                 i++;
+            }
+        }
+
+        return -1;
+    }
+}
+
+
+
+
+
+// Approach 3 - Binary Search (If one arr is much longer than other, it's most efficient than other two approaches)
+// T.C. - O(mlogn); where m is the length of the shorter array and n is the length of the longer array
+// S.C. - O(1)
+class Solution {
+    public boolean binarySearch(int target, int[] nums){
+        int l = 0;
+        int r = nums.length - 1;
+
+        while(l <= r){
+            int mid = l + (r - l)/2;
+
+            if(nums[mid] > target){
+                r = mid - 1;
+            }
+            else if(nums[mid] < target){
+                l = mid + 1;
+            }
+            else{
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public int getCommon(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        
+        // Binary search should be done on the larger array
+        // If nums1 is longer, call getCommon with the arrays swapped
+        if(m > n){
+            return getCommon(nums2, nums1);
+        }
+        
+        // Searching for each element of nums1 in nums2
+        // Return the first common element found
+        for(int num : nums1){
+            if(binarySearch(num, nums2)){
+                return num;
             }
         }
 
