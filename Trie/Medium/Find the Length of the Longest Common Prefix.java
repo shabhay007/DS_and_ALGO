7f7d84969182523c17f8fa2eae1d3b95b00519b3
Abhay Sharma <shabhay007@gmail.com>
2026-05-21 16:52:20 +0530

    Added other approach to the problem 'Find the Length of the Longest Common Prefix'

--- Trie/Medium/Find the Length of the Longest Common Prefix.java
+++ Trie/Medium/Find the Length of the Longest Common Prefix.java
@@ -138,3 +138,72 @@ class Solution {
         return result;
     }
 }
+
+
+
+
+
+// Approach 4 - Using Prefix Tree (TRIE)
+// T.C : O(m⋅d + n⋅d)
+// S.C : O(m⋅d)
+class TrieNode {
+    TrieNode[] children = new TrieNode[10];  // 0, 1, 2...9
+}
+
+class Solution {
+
+    public TrieNode getTrieNode() {
+        return new TrieNode();
+    }
+
+    public void insert(int num, TrieNode root) {
+        TrieNode crawl = root;
+        String numStr = Integer.toString(num);
+
+        for (char ch : numStr.toCharArray()) {
+            int idx = ch - '0';
+            if (crawl.children[idx] == null) {
+                crawl.children[idx] = getTrieNode();
+            }
+            crawl = crawl.children[idx];
+        }
+    }
+
+    // Returns the length of the longest prefix
+    public int search(int num, TrieNode root) {
+        TrieNode crawl = root;
+        String numStr = Integer.toString(num);
+        int length = 0;
+
+        for (char ch : numStr.toCharArray()) {
+            int idx = ch - '0';
+
+            if (crawl.children[idx] != null) {
+                length++;
+                crawl = crawl.children[idx];
+            } 
+            else {
+                break;
+            }
+        }
+
+        return length;
+    }
+
+    public int longestCommonPrefix(int[] arr1, int[] arr2) {
+        TrieNode root = getTrieNode();  // TRIE
+
+        // Insert all elements of arr1 into the trie
+        for (int num : arr1) {
+            insert(num, root);
+        }
+
+        // Search for the longest common prefix in arr2
+        int result = 0;
+        for (int num : arr2) {
+            result = Math.max(result, search(num, root));
+        }
+
+        return result;
+    }
+}
