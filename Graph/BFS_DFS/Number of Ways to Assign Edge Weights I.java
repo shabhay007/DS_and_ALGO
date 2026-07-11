4dcaa2172ac953db2fbe35c61721ebcde55a1c01
Abhay Sharma <shabhay007@gmail.com>
2026-07-11 16:49:15 +0530

    Added problem 'Number of Ways to Assign Edge Weights I' in Graph DFS

--- Graph/BFS_DFS/Number of Ways to Assign Edge Weights I.java
+++ Graph/BFS_DFS/Number of Ways to Assign Edge Weights I.java
@@ -120,3 +120,78 @@ class Solution {
         return (int) binaryExponentiation(2, maxEdge-1);
     }
 }
+
+
+
+
+
+
+// Approach 3 - Depth of Graph + Binary Exponentiation
+// T.C. - O(n)
+// S.C. - O(n)
+class Solution {
+    int mod = (int) 1e9 + 7;
+
+    public int binaryExponentiation(int num, int power){
+        if(power == 0){
+            return 1;
+        }
+
+        long half = binaryExponentiation(num, power/2);
+        long result = (half * half) % mod;
+
+        if(power % 2 != 0){
+            result = (result * num) % mod;
+        }
+
+        return (int) result;
+    }
+
+    public int getMaxDepth(int root, List<List<Integer>> adj, int parent){
+        int maxDepth = 0;
+
+        for(int ngbr : adj.get(root)){
+            if(ngbr == parent){
+                continue;
+            }
+
+            maxDepth = Math.max(maxDepth, getMaxDepth(ngbr, adj, root) + 1);
+        }
+
+        return maxDepth;
+    }
+
+    public int assignEdgeWeights(int[][] edges) {
+        int n = edges.length + 1;
+
+        // building the graph
+        List<List<Integer>> adj = new ArrayList<>();
+
+        for(int i = 0; i<=n; i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int[] edge : edges){ // O(n)
+            int u = edge[0];
+            int v = edge[1];
+
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+
+        // finding the max depth
+        int d = getMaxDepth(1, adj, -1); // O(n)
+
+        /**
+            So, there will be total 2^d ways including odd and even
+            so, odd + even = total
+                -> 2odd = 2^d
+                -> odd = 2^(d-1)
+        */
+
+        // max depth will be d and in worst case (skewed tree) it's O(log(n))
+        int result = binaryExponentiation(2, d-1); // O(log(n))
+
+        return result;
+    }
+}
