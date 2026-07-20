57066a499f3c2081110d867e1a84cd809bd0d69a
Abhay Sharma <shabhay007@gmail.com>
2026-07-20 23:41:48 +0530

    Added other approach to the problem 'Shift 2D Grid'

--- Matrix/Easy/Shift 2D Grid.java
+++ Matrix/Easy/Shift 2D Grid.java
@@ -27,3 +27,38 @@ class Solution {
         return Arrays.stream(result).map(r -> Arrays.stream(r).boxed().toList()).toList();
     }
 }
+
+
+
+
+
+// Approach 2 - Reverse Trick
+// T.C : O(n), n = row*col
+// S.C : O(1)
+class Solution {
+    public void reverse(int i, int j, int[][] grid, int col){
+        while(i < j){
+            int temp = grid[i/col][i%col];
+            grid[i/col][i%col] = grid[j/col][j%col];
+            grid[j/col][j%col] = temp;
+            i++;
+            j--;
+        }
+    }
+
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        int m = grid.length;
+        int n  = grid[0].length;
+        int N = m * n;
+
+        k = k % N;
+
+        if(k != 0){
+            reverse(0, N-1, grid, n);
+            reverse(0, k-1, grid, n);
+            reverse(k, N-1, grid, n); 
+        }
+
+        return Arrays.stream(grid).map(row -> Arrays.stream(row).boxed().toList()).toList();
+    }
+}
