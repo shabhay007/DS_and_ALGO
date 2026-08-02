ca16d4d5fd6df88c6795e07efdf6c85bcc1b9936
Abhay Sharma <shabhay007@gmail.com>
2026-08-02 15:37:57 +0530

    Added problem 'Stone Game' in Game Theory

---
+++ Game Theory/Medium/Stone Game.java
@@ -0,0 +1,44 @@
+// LeetCode - 877
+
+
+
+// Approach 1 - DP (Memoization)
+// T.C. - O(n^2)
+// S.C. - O(n^2)
+class Solution {
+    int n;
+
+    public int solve(int i, int j, int[] piles, int[][] dp){
+        if(i > j){
+            return 0;
+        }
+
+        if(i == j){
+            return piles[i];
+        }
+
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+
+        int aliceI = piles[i] + Math.min(solve(i+2, j, piles, dp), solve(i+1, j-1, piles, dp));
+        int aliceJ = piles[j] + Math.min(solve(i+1, j-1, piles, dp), solve(i, j-2, piles, dp));
+
+        return dp[i][j] = Math.max(aliceI, aliceJ);
+    }
+
+    public boolean stoneGame(int[] piles) {
+        n = piles.length;
+        int totalStones = Arrays.stream(piles).sum();
+
+        int[][] dp = new int[n][n];
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
+        }
+
+        int aliceStones = solve(0, n-1, piles, dp);
+        int bobStones = totalStones - aliceStones;
+
+        return aliceStones >= bobStones;
+    }
+}
