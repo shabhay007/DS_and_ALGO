5bd17a95b7d07a82022cf54d9bfe980488fb1d82
Abhay Sharma <shabhay007@gmail.com>
2026-08-05 15:48:07 +0530

    Added most optimal approach to the problem 'Remove Methods From Project'

--- Graph/BFS_DFS/Remove Methods From Project.java
+++ Graph/BFS_DFS/Remove Methods From Project.java
@@ -149,3 +149,81 @@ class Solution {
         return list;
     }
 }
+
+
+
+
+
+
+// Approach 3 - BFS + Kahn's Algorithm Concept
+// T.C. - O(n + m)
+// S.C. - O(n + m)
+class Solution {
+    public void bfs(int curr, List<List<Integer>> adj, boolean[] suspect, int[] indegree){
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(curr);
+        suspect[curr] = true;
+
+        while(!q.isEmpty()){
+            int node = q.poll();
+
+            for(int ngbr : adj.get(node)){
+                indegree[ngbr]--;
+
+                if(!suspect[ngbr]){
+                    q.offer(ngbr);
+                    suspect[ngbr] = true;
+                }
+            }
+        }
+    }
+
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+        // STEP 1: Graph Building
+        List<List<Integer>> adj = new ArrayList<>();
+        int[] indegree = new int[n];
+
+        for(int i = 0; i<n; i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int[] inv : invocations){
+            int a = inv[0];
+            int b = inv[1];
+
+            adj.get(a).add(b);
+            indegree[b]++;
+        }
+
+        // STEP 2: performing dfs from kth node
+        boolean[] suspect = new boolean[n];
+        bfs(k, adj, suspect, indegree);
+
+        // STEP 3: checking dependency of the suspects
+        boolean isRemovalPossible = true;
+        for(int i = 0; i<n; i++){
+            if(suspect[i] && indegree[i] != 0){
+                isRemovalPossible = false;
+                break;
+            }
+        }
+
+        // STEP 4: remaining methods
+        List<Integer> list = new ArrayList<>();
+
+        if(!isRemovalPossible){
+            for(int i = 0; i<n; i++){
+                list.add(i);
+            }
+        }
+        else{
+            for(int i = 0; i<n; i++){
+                if(!suspect[i]){
+                    list.add(i);
+                }
+            }
+        }
+
+        return list;
+    }
+}
